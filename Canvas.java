9d0144a232f298c595866ca3b68e0c1af961c02d
JosephSalerno <56265826+JosephSalerno@users.noreply.github.com>
2019-11-07 22:36:09 -0500

    Updated slightly

--- Canvas.java
+++ Canvas.java
@@ -1,3 +1,13 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import javax.swing.JPanel;
+import java.awt.event.*;
+import shapes.AllShapes;
+
 /**
  * A <code>Canvas</code> class 
  * @author JosephSalerno
@@ -7,86 +17,143 @@
  * Project: 4
  */
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-
-public class Canvas extends JPanel implements MouseListener,
-                                              MouseMotionListener {
-
-   //Point mouseLocation;
-   //Point clickLocation;
+public class Canvas extends JPanel implements MouseListener, MouseMotionListener {
    
-   Point startLocation;
-   Point endLocation;
-
+   // Variables
+   private Point posStart;
+   private Point posEnd;   
+   private java.awt.Rectangle drawRect;
+   public boolean isFilled;
+   public String newColor;
+   public String shapeToDraw;
+  
+   
+   // Final variables
+   final private Color colorSelect = new Color(0, 0, 0);
+   
+   // Constructors and setup methods
    public Canvas() {
       super();
+      this.setOpaque(true);
+      this.setBackground(Color.WHITE);
       
-      addMouseListener(this);
+      // No selection rectangle to draw
+      drawRect = null;
+      
+      // Listen for mouse movement or input
+      addMouseListener(this);      
       addMouseMotionListener(this);
+      
+      
    }
    
+   // Listeners
    @Override
    public void paintComponent(Graphics g) {
       super.paintComponent(g);
       
-      Graphics2D painterG = (Graphics2D) g;
-      
-      if (startLocation != null) {
-         painterG.setColor(Color.GREEN);
-         int width = Math.abs(endLocation.x - startLocation.x);
-         int height = Math.abs(endLocation.y - startLocation.y);
-         int left = Math.min(startLocation.x, endLocation.x);
-         int top = Math.min(startLocation.y, endLocation.y);
-         painterG.drawRect(left, top, width, height);
+      // Draw any shapes in the shape holder here
+      // Draw drag rectangle if it is there
+      if (drawRect != null) {
+         g.drawRect((int)drawRect.getX(), (int)drawRect.getY(),
+                    (int)drawRect.getWidth(), (int)drawRect.getHeight());
+         
+         // Draw the current shape here
+         
       }
       
-      //if (mouseLocation != null) {
-      //   painterG.setColor(Color.BLUE);
-      //   painterG.fillOval(mouseLocation.x, mouseLocation.y, 50, 50);
-      //}
-      //if (clickLocation != null) {
-      //   painterG.setColor(Color.RED);
-      //   painterG.fillRect(clickLocation.x, clickLocation.y, 50, 50);
-      //}
    }
    
-   public void mouseClicked(MouseEvent e) {
-      //clickLocation = e.getPoint();
-      //repaint();
-      //
-   }
+   // Highly recommended to create this method for assisting in the paintComponent method
+   //private void drawGenericShape(Graphics g, GeometricShape s) {
+      // Draw any sort of shape to the canvas
+      
+   //}
    
+   // Needed for mouse listeners
+   @Override
    public void mouseEntered(MouseEvent e) {
-      
    }
-   
+
+   @Override
    public void mouseExited(MouseEvent e) {
-      
    }
    
-   public void mousePressed(MouseEvent e) {
-      startLocation = e.getPoint();
-      endLocation = e.getPoint();
-      
-      repaint();
-   }
-   
-   public void mouseReleased(MouseEvent e) {
-      
-   }
-   
-   public void mouseDragged(MouseEvent e) {
-      endLocation = e.getPoint();
-      repaint();
-      
-   }
-   
+   @Override
    public void mouseMoved(MouseEvent e) {
-      //mouseLocation = e.getPoint();
-      //repaint();
+      
+      
+   }
+   
+   @Override
+   public void mousePressed(MouseEvent e) {
+      if (e.getButton() == MouseEvent.BUTTON1) {
+         posStart = new Point(e.getX(), e.getY());
+         posEnd = new Point(e.getX(), e.getY());
+         
+         // Create new shape here
+         
+         updateRectangle();
+         
+        
+      }
+   }
+
+   @Override
+   public void mouseReleased(MouseEvent e) {
+      if (e.getButton() == MouseEvent.BUTTON1) {
+         posEnd.setLocation(e.getX(), e.getY());
+         updateRectangle();
+         
+         // Add the shape to the shapeholder here        
+         
+         // Free up the draw variables
+         drawRect = null;
+         posStart = null;
+         posEnd = null;
+         
+      }
+      
+   }
+   
+   @Override
+   public void mouseDragged(MouseEvent e) {
+      if (drawRect != null) {
+         posEnd.setLocation(e.getX(), e.getY());
+         updateRectangle();
+      }
    }
 
 
+
+   // Needed for mouse listeners
+   @Override
+   public void mouseClicked(MouseEvent e) {
+	 //clickLocation = e.getPoint();
+	 updateRectangle();
+      
+   }
+   
+   // Updates the selection rectangle based on the first and current mouse positions
+   public void updateRectangle() {
+      
+      if (drawRect == null) {
+         drawRect = new java.awt.Rectangle(0, 0, 0, 0);
+      }
+      
+      int width = (int)Math.abs(posEnd.getX() - posStart.getX());
+      int height = (int)Math.abs(posEnd.getY() - posStart.getY());
+      int left = (int)Math.min(posStart.getX(), posEnd.getX());
+      int top = (int)Math.min(posStart.getY(), posEnd.getY());
+      
+      // Set up rectangle to the correct four corners      
+      drawRect.setLocation(left, top);
+      
+      drawRect.setSize(width, height);
+      
+      // Let paintComponent handle this later
+      repaint();
+   }
+   
+   
 }
